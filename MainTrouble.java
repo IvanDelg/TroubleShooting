b9de62969bed1f03afbde7d64cf1f774a5904bb9
IvanDelg <31767956+IvanDelg@users.noreply.github.com>
2018-09-25 00:42:43 -0500

    Add files via upload

--- MainTrouble.java
+++ MainTrouble.java
@@ -1,10 +1,7 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-
-import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -30,7 +27,7 @@ public class MainTrouble extends JFrame{
 			}
 	};
 	String wordFor;
-	JButton button[]=new JButton[11];
+	JButton button[]=new JButton[12];
 	JPanel panel=new JPanel();
 	public MainTrouble() {
 		add(panel);
@@ -39,10 +36,10 @@ public class MainTrouble extends JFrame{
 		setTitle("Trouble Shooting");
 		setLocation(WIDTH/3, HEIGHT/3);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		for(int x=0,j=0,y=0;x<11;x++) {
+		for(int x=0,j=0,y=0;x<=11;x++) {
 			button[x]=new JButton();
-			button[x].setEnabled(true);
-			button[x].setVisible(true);
+			button[x].setEnabled(false);
+			button[x].setVisible(false);
 			button[x].setSize(300,200);
 			button[x].setText(simpleWords[y][j]);
 			button[x].setName(simpleWords[y][j]);
@@ -54,41 +51,52 @@ public class MainTrouble extends JFrame{
 							}
 						}
 					);
+			panel.add(button[x]);
+			if(j<3){
+				j++;
+			}
 			if((x+1)%4==0) {
 				y++;
 				j=0;
-			}
-			if(j<3){
-				j++;
 			}
 		}
 		start();
 	}
 	private void start() {
+		int xxx=0;
 		while(true) {
-			mainMenu();
-		}
-	}
-	private void mainMenu() {
 			if(wordFor!=null) {
 				menuFor(wordFor);
 			}else {
-				for(int x=0;x<=11;x++) {
-					panel.add(button[x]);
-					button[x].setEnabled(true);
-					button[x].setVisible(true);
-					x=+4;
+				if(xxx>0){
+				}else{
+				mainMenu();
 				}
+				xxx++;
+			}
+		}
+	}
+	private void mainMenu() {
+			for(int x=0;x<11;x++) {
+				button[x].setEnabled(true);
+				button[x].setVisible(true);
+				x+=3;
 			}
 	}
+	private void waitForIt() {
+		while(wordFor==null) {}
+	}
 	private void menuFor(String word) {
+		System.out.println(word);
 		setTitle(word+" Problems");
 		int wordCol;
 		for(int x=0;x<simpleWords.length;x++) {
 			if(word.equals(simpleWords[x][0])) {
 				wordCol=x;
+				System.out.println(word);
 			}
 		}
+		
 		
 	}
 }
